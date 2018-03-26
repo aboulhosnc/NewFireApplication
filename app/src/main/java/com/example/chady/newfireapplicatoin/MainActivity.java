a957eb2bbe2f2037ce00cd718cc6b0a51475aabf
Chady Aboulhosn <aboulhosnc@vcu.edu>
2018-03-25 22:50:17 -0400

    Authentication and log in with email demo from video

--- app/src/main/java/com/example/chady/newfireapplicatoin/MainActivity.java
+++ app/src/main/java/com/example/chady/newfireapplicatoin/MainActivity.java
@@ -1,30 +1,97 @@
 package com.example.chady.newfireapplicatoin;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    //reference to the database
+    DatabaseReference myref;
+
+    // reference to the authentication
+    private FirebaseAuth mAuth;
+    // makes a check if user is authenticated
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    private EditText emailText;
+    private EditText passText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        emailText = (EditText) findViewById(R.id.userEmail);
+        passText = (EditText) findViewById(R.id.userPassword);
+
+        // reference to mauthentication
+
+        mAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                // new activity once you are logged in
+                // check if user is authenticated
+
+                if(firebaseAuth.getCurrentUser()!=null)
+                {
+                    Intent intent = new Intent(MainActivity.this, NewActivity.class);
+                    startActivity(intent);
+                }
+
+
+            }
+        };
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(authStateListener);
+    }
+
+    public void loginButtonClicked (View view)
+    {
+        String email = emailText.getText().toString();
+        String pass = passText.getText().toString();
+
+        if(TextUtils.isEmpty(email) | TextUtils.isEmpty(pass))
+        {
+            Toast.makeText(this, "Enter both values", Toast.LENGTH_LONG).show();
+
+        }
+        else
+        {
+            mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(!task.isSuccessful())
+                    {
+                        Toast.makeText(MainActivity.this, "Incorrect username or password",  Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            });
+        }
     }
 }
---
+++ app/src/main/java/com/example/chady/newfireapplicatoin/NewActivity.java
@@ -0,0 +1,27 @@
+package com.example.chady.newfireapplicatoin;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class NewActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+
+    public void onSignOutClick(View view)
+    {
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.signOut();
+        startActivity(new Intent(NewActivity.this, MainActivity.class));
+    }
+}
